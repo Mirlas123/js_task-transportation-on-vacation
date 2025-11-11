851c9dd7b8f2a1c30720dec6df7cbd10f7ce9b9a
Mirlas <mirlas@gmail.com>
2025-11-11 14:03:18 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const longt = 7;
+  const longdisk = 50;
+  const smallt = 3;
+  const smalltdisk = 20;
+
+  if (days >= longt) {
+    return days * 40 - longdisk;
+  }
+
+  if (days >= smallt) {
+    return days * 40 - smalltdisk;
+  }
+
+  return days * 40;
 }
 
 module.exports = calculateRentalCost;
